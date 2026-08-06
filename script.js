d0fc7b5df86bfeb2d9a680ca2241e5848a940995
vaibhsnh <vaibhsnh@gmail.com>
2026-08-06 23:51:23 +0530

    made UI/UX changes in landing-page

--- script.js
+++ script.js
@@ -7,6 +7,18 @@ let setTheme = () => {
 };
 setTheme();
 
-window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", function(){
-  setTheme();
-})
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", function () {
+    setTheme();
+  });
+
+const toggleTheme = document.querySelector(".theme-btn");
+
+toggleTheme.addEventListener("click", function () {
+  if (document.body.getAttribute("data-theme")=== "dark") {
+    document.body.setAttribute("data-theme", "light");
+  } else {
+    document.body.setAttribute("data-theme", "dark");
+  }
+});
