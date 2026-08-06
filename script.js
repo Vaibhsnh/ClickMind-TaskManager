c53fa6ca9070abee07db5fd9bd8ea4aa164a2498
vaibhsnh <vaibhsnh@gmail.com>
2026-08-07 00:05:58 +0530

    made UI/UX changes in landing-page

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let setThemeDarkorLight = () => {
+  const logo = document.querySelector(".logo img");
   if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
     document.body.setAttribute("data-theme", "dark");
+    logo.setAttribute("src", "assets/logo/ClickMind_LightLogo.png");
   } else {
     document.body.setAttribute("data-theme", "light");
+    logo.setAttribute("src", "assets/logo/ClickMind_DarkLogo.png");
   }
 };
 setThemeDarkorLight();
@@ -15,12 +18,15 @@ window
 
 let toggleTheme = () => {
   const toggleTheme = document.querySelector(".theme-btn");
+  const logo = document.querySelector(".logo img");
 
   toggleTheme.addEventListener("click", function () {
     if (document.body.getAttribute("data-theme") === "dark") {
       document.body.setAttribute("data-theme", "light");
+      logo.setAttribute("src", "assets/logo/ClickMind_DarkLogo.png");
     } else {
       document.body.setAttribute("data-theme", "dark");
+      logo.setAttribute("src", "assets/logo/ClickMind_LightLogo.png");
     }
   });
 };
